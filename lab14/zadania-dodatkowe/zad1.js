58071f12e38b16628083032346ed30ff9e9cdad0
Hubert Kowalski <37780367+hubcio2115@users.noreply.github.com>
2022-06-13 17:55:58 +0200

    Update zad1.js

--- lab14/zadania-dodatkowe/zad1.js
+++ lab14/zadania-dodatkowe/zad1.js
@@ -1,16 +1,20 @@
 "use strict";
+
+// To ogólnie nie działa, nie wiem jak to zrobić, jak masz odpowiedź to zrób PRa
+
 const getRandomNumber = () => {
-    return Math.floor(Math.random() * 1000);
+  return Math.floor(Math.random() * 1000);
 };
 const getNumber = (cb) => {
-    setTimeout(() => {
-        cb(getRandomNumber());
-    }, getRandomNumber() * 5);
+  setTimeout(() => {
+    cb(getRandomNumber());
+  }, getRandomNumber() * 5);
 };
 const asyncSort = (dim) => {
-    const unsortedArray = [...Array(dim).keys()].map(async (_) => {
-        return getNumber((n) => n + 1);
-    });
-    console.log(unsortedArray);
+  const unsortedArray = [...Array(dim).keys()].map((_) => {
+    return getNumber((n) => n + 1);
+  });
+
+  console.log(unsortedArray);
 };
 asyncSort(5);

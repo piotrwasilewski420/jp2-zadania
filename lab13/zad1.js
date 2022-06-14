2bda6e8f63a879b7f07bf200224469375fe7eb48
Hubert Kowalski <37780367+hubcio2115@users.noreply.github.com>
2022-06-14 17:56:43 +0200

    fix lab13/zad1

--- lab13/zad1.js
+++ lab13/zad1.js
@@ -1,15 +1,20 @@
 "use strict";
-const func1 = async (x, y) => x + y;
-const func2 = async (x, y) => x * y;
-const cb = (x, y) => x + y;
+const func1 = (init, cb) => {
+  cb(init + 10);
+};
 
-const [x1, y1] = [10, 10];
-const [x2, y2] = [5, 5];
+const func2 = (init, cb) => {
+  cb(init + 20);
+};
 
-const oneAfterAnother = async (func1, func2, cb) => {
-  Promise.all([func1(x1, y1), func2(x2, y2)]).then((res) => {
-    const [arg1, arg2] = res;
-    console.log(cb(arg1, arg2));
+const cb = (init) => init + 30;
+
+const oneAfterAnother = (func1, func2, cb) => {
+  func1(0, (result) => {
+    func2(result, (result1) => {
+      console.log(cb(result1));
+    });
   });
 };
+
 oneAfterAnother(func1, func2, cb);
